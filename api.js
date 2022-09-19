6d4516ae9a2e50d72ec72ca7c7758492804268a2
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-09-20 02:53:46 +0500

    del by id through params in postman

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dbconnect =require("./mongodb")
+const mongodb = require("mongodb")
 const app = express()
 app.use(express.json())
 app.get("/", async (req,res)=>{
@@ -12,13 +13,13 @@ res.send(data)
 })
 
 
-app.post("/" , async (req,res)=>{
-    // console.log(req.body)
-    let data =await dbconnect();
-let result = await  data.insertOne(req.body)
+// app.post("/" , async (req,res)=>{
+//     // console.log(req.body)
+//     let data =await dbconnect();
+// let result = await  data.insertOne(req.body)
     
-    res.send(result)
-})
+//     res.send(result)
+// })
 
 app.put("/:name", async (req,res)=>{
     let data = await dbconnect();
@@ -32,4 +33,13 @@ let result = await data.updateOne(
 //     console.log(req.body)
 // res.send({result:"upadted"})
 // })
+
+
+app.delete("/:id",async (req,res)=>{
+    console.log(req.params.id)
+    let data =await dbconnect()
+   let result = await  data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+    res.send(resut)
+
+})
 app.listen(4000)
