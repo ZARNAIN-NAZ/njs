8ebef72617e490eeb4750e6763863e668e6c7b75
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-09-21 20:36:28 +0500

    put,get,delete,update mongoose with postman.

--- index.js
+++ index.js
@@ -325,13 +325,31 @@ const express = require("express")
 require("./config")
 const product =require("./product")
 const app = express()
-// app.use(express.json())
+
+app.use(express.json())
 app.post("/create",async(req,res)=>{
-    // let data = new  product(req.body)
-    // let result =await data.save()
-    // console.log(req.body)
-    // res.send(result)
+    let data = new  product(req.body)
+    let result =await data.save()
+    console.log(req.body)
+    res.send(result)
     // res.send("finally")
     // res.send("done")
 })
-app.listen(5000)
+
+app.get("/list",async(req,res)=>{
+let data = await  product.find()
+res.send(data)
+})
+
+app.put("/update/:_id",async(req,res)=>{
+    console.log(req.params)
+    // let data = await  product.deleteOne(req.params)
+    let data = await  product.updateOne(
+         req.params,
+         {
+            $set:req.body
+        })
+    res.send(data)
+
+    })
+app.listen(5000) 
