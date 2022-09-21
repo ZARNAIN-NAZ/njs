af0e1861f9710e31c3118114c10f1453156c8c66
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-09-22 02:07:53 +0500

    os module

--- index.js
+++ index.js
@@ -371,24 +371,32 @@
 // })
 // app.listen(5000)
 //---------------------upload file with multer----------------
-const express= require("express")
-const multer = require("multer")
-const app = express()
+// const express= require("express")
+// const multer = require("multer")
+// const app = express()
 
-const upload = multer({
-    storage:multer.diskStorage({
-        destination:function(req,file,cb)
-        {
-            cb(null,"uploads")
-        },
-            filename:function(req,file,cb)
-        {
-            cb(null,file.fieldname+ "-" +Date.now()+ ".jpg")
-        }
-    })
-}).single("user_file");
+// const upload = multer({
+//     storage:multer.diskStorage({
+//         destination:function(req,file,cb)
+//         {
+//             cb(null,"uploads")
+//         },
+//             filename:function(req,file,cb)
+//         {
+//             cb(null,file.fieldname+ "-" +Date.now()+ ".jpg")
+//         }
+//     })
+// }).single("user_file");
 
-app.post("/upload",upload , (req,res)=>{
-    res.send("file uploading")
-})
-app.listen(5000)
+// app.post("/upload",upload , (req,res)=>{
+//     res.send("file uploading")
+// })
+// app.listen(5000)
+//------------------------------------os module-----------------------
+const os  = require("os")
+// console.log(os.arch())
+// console.log(os.freemem()/(1024*1024*1024))
+// console.log(os.totalmem()/(1024*1024*1024))
+// console.log(os.hostname())
+// console.log(os.platform())
+console.log(os.userInfo())
