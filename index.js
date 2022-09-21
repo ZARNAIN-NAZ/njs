211cad38d327d56611b964f5d6aa11f32b711408
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2022-09-22 01:09:08 +0500

    serach api in nodejs with mongodb

--- index.js
+++ index.js
@@ -321,35 +321,52 @@
 // }
 // FindInDb()
 // ----------------api connenction with diiff pages--------
-const express = require("express")
+// const express = require("express")
+// require("./config")
+// const product =require("./product")
+// const app = express()
+
+// app.use(express.json())
+// app.post("/create",async(req,res)=>{
+//     let data = new  product(req.body)
+//     let result =await data.save()
+//     console.log(req.body)
+//     res.send(result)
+//     // res.send("finally")
+//     // res.send("done")
+// })
+
+// app.get("/list",async(req,res)=>{
+// let data = await  product.find()
+// res.send(data)
+// })
+
+// app.put("/update/:_id",async(req,res)=>{
+//     console.log(req.params)
+//     // let data = await  product.deleteOne(req.params)
+//     let data = await  product.updateOne(
+//          req.params,
+//          {
+//             $set:req.body
+//         })
+//     res.send(data)
+
+//     })
+// app.listen(5000) 
+//---------------search api's with nodejs mongodb----------------------------------------
+const express = require('express')
 require("./config")
-const product =require("./product")
-const app = express()
-
+const product= require("./product")
+const app = express();
 app.use(express.json())
-app.post("/create",async(req,res)=>{
-    let data = new  product(req.body)
-    let result =await data.save()
-    console.log(req.body)
-    res.send(result)
-    // res.send("finally")
-    // res.send("done")
-})
-
-app.get("/list",async(req,res)=>{
-let data = await  product.find()
-res.send(data)
-})
-
-app.put("/update/:_id",async(req,res)=>{
-    console.log(req.params)
-    // let data = await  product.deleteOne(req.params)
-    let data = await  product.updateOne(
-         req.params,
-         {
-            $set:req.body
-        })
+app.get("/search/:key" , async (req,res)=>{
+    // cosnole.log(req.params.key)
+    let data =await  product.find(
+        {
+            "$or":[{"name":{$regex:req.params.key}},{"brand":{$regex:req.params.key}}]
+        }
+    )
+    // res.send("serach done")
     res.send(data)
-
-    })
-app.listen(5000) 
+})
+app.listen(5000)
